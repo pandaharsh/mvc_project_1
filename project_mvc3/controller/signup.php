5242165e792cd5929369f14bfd28b4460f1dafa1
pandaharsh <122609133+pandaharsh@users.noreply.github.com>
2023-04-11 17:32:08 +0530

    add Changes

---
+++ project_mvc3/controller/signup.php
@@ -0,0 +1,27 @@
+<?php
+
+include("./core/controller.php");
+
+class signup extends controller
+{
+
+    protected $username;
+    protected $title;
+    protected $description;
+
+    public function __construct()
+    {
+
+        $this->username = $_POST['username'];
+        $this->title = $_POST['title'];
+        $this->description = $_POST['description'];
+
+        $this->view("signup");
+
+        $this->model($this->username, $this->title, $this->description);
+
+    }
+
+}
+
+?>
